69a18085b2d4028da5d2e907976b860d684daea3
CGM <cgmajoor@gmail.com>
2024-04-22 15:01:07 +0200

    add tests for viewmodel and usecase

--- CountriesApp/Screens/Countries/CountriesViewModel.swift
+++ CountriesApp/Screens/Countries/CountriesViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class CountriesViewModel {
+class CountriesViewModel: CountriesViewModelProtocol {
 
     var countries: [Country] = []
 
---
+++ CountriesApp/Screens/Countries/CountriesViewModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CountriesViewModelProtocol.swift
+//  CountriesApp
+//
+//  Created by Ceren Majoor on 22/04/2024.
+//
+
+import Foundation
+
+protocol CountriesViewModelProtocol {
+    func loadCountries() 
+}
